f4c378742f87f09df398e3dab7f085e867111e40
Ryan Dahl <ry@tinyclouds.org>
2019-07-01 14:23:34 -0400

    rerun-if-changed

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod prebuild;
 pub use prebuild::*;
+use std::collections::HashSet;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
@@ -45,6 +46,11 @@ fn write_args(path: &PathBuf, contents: &str) {
 pub fn build(c: &Config) {
   let gn_out_dir = out_dir().join("gn_out");
 
+  let deps = ninja_deps(&gn_out_dir);
+  for dep in deps.iter() {
+    println!("cargo:rerun-if-changed={}", dep);
+  }
+
   let status = Command::new(gn_path())
     .arg(format!("--root={}", c.root))
     .arg("gen")
@@ -76,6 +82,30 @@ pub fn build(c: &Config) {
   );
 }
 
+pub fn ninja_deps(gn_out_dir: &PathBuf) -> HashSet<String> {
+  let output = Command::new(ninja_path())
+    .arg("-C")
+    .arg(gn_out_dir)
+    .arg("-t")
+    .arg("deps")
+    .output()
+    .expect("ninja -t deps failed");
+
+  let stdout = std::str::from_utf8(&output.stdout).unwrap();
+  ninja_deps_parse(&stdout)
+}
+
+pub fn ninja_deps_parse(s: &str) -> HashSet<String> {
+  let mut out = HashSet::new();
+  for line in s.lines() {
+    if line.starts_with("    ") {
+      let filename = String::from(line.trim_start());
+      out.insert(filename);
+    }
+  }
+  out
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -88,5 +118,30 @@ mod tests {
   #[test]
   fn ninja_exists() {
     assert!(ninja_path().exists());
+  }
+
+  const NINJA_DEPS_OUTPUT: &'static str = r#"
+obj/foo/foo.o: #deps 2, deps mtime 1562002734 (VALID)
+    ../../../../../../example/src/foo.cc
+    ../../../../../../example/src/hello.h
+
+obj/hello/hello.o: #deps 2, deps mtime 1562002734 (VALID)
+    ../../../../../../example/src/hello.cc
+    ../../../../../../example/src/hello.h
+  "#;
+
+  #[test]
+  fn test_parse_ninja_deps_output() {
+    let foo_cc = "../../../../../../example/src/foo.cc".to_string();
+    let hello_cc = "../../../../../../example/src/hello.cc".to_string();
+    let hello_h = "../../../../../../example/src/hello.h".to_string();
+    let blah_h = "../../../../../../example/src/blah.h".to_string();
+
+    let set = ninja_deps_parse(NINJA_DEPS_OUTPUT);
+
+    assert!(set.contains(&foo_cc));
+    assert!(set.contains(&hello_cc));
+    assert!(set.contains(&hello_h));
+    assert!(!set.contains(&blah_h));
   }
 }
