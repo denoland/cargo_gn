0a2fd6c3911e3f1dfc94c843b2cef5842e872fba
Ryan Dahl <ry@tinyclouds.org>
2019-07-18 12:33:52 -0400

    Remove canonicalize

--- src/prebuild.rs
+++ src/prebuild.rs
@@ -5,11 +5,22 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn out_dir() -> PathBuf {
+  // The OUT_DIR is going to be a crate-specific directory like
+  // "target/debug/build/cargo_gn_example-eee5160084460b2c"
+  // But we want to share the GN build amongst all crates
+  // and return the path "target/debug". So to find it, we walk up three
+  // directories.
+  // TODO(ry) This is quite brittle - if Cargo changes the directory structure
+  // this could break.
   let out_dir = env::var("OUT_DIR").unwrap();
   PathBuf::from(out_dir)
-    .join("../../..")
-    .canonicalize()
+    .parent()
     .unwrap()
+    .parent()
+    .unwrap()
+    .parent()
+    .unwrap()
+    .to_owned()
 }
 
 pub fn is_debug() -> bool {
