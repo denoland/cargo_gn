5565d5dfa977942dda29eb829ee451fd2acca2bc
Ryan Dahl <ry@tinyclouds.org>
2019-07-21 18:22:19 -0400

    Remove built-in ninja/gn builds (#13)

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,47 @@
-mod prebuild;
-pub use prebuild::*;
 use std::collections::HashSet;
+use std::env;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
-pub use which::which;
 
-static GN_PATH: &'static str = env!("GN_PATH");
-static NINJA_PATH: &'static str = env!("NINJA_PATH");
-
-pub fn gn_path() -> PathBuf {
-  PathBuf::from(GN_PATH)
+pub fn out_dir() -> PathBuf {
+  // The OUT_DIR is going to be a crate-specific directory like
+  // "target/debug/build/cargo_gn_example-eee5160084460b2c"
+  // But we want to share the GN build amongst all crates
+  // and return the path "target/debug". So to find it, we walk up three
+  // directories.
+  // TODO(ry) This is quite brittle - if Cargo changes the directory structure
+  // this could break.
+  let out_dir = env::var("OUT_DIR").unwrap();
+  PathBuf::from(out_dir)
+    .parent()
+    .unwrap()
+    .parent()
+    .unwrap()
+    .parent()
+    .unwrap()
+    .to_owned()
 }
 
-pub fn ninja_path() -> PathBuf {
-  PathBuf::from(NINJA_PATH)
+pub fn is_debug() -> bool {
+  // Cargo sets PROFILE to either "debug" or "release", which conveniently
+  // matches the build modes we support.
+  let m = env::var("PROFILE").unwrap();
+  if m == "release" {
+    false
+  } else if m == "debug" {
+    true
+  } else {
+    panic!("unhandled PROFILE value {}", m)
+  }
+}
+
+fn gn() -> String {
+  env::var("GN").unwrap_or_else(|_| "gn".to_owned())
+}
+
+fn ninja() -> String {
+  env::var("NINJA").unwrap_or_else(|_| "ninja".to_owned())
 }
 
 // TODO(ry) debug_args and release_args should be Vec<String> ?
@@ -25,7 +52,10 @@ pub fn maybe_gen(root: &str, debug_args: &str, release_args: &str) -> PathBuf {
     let args = if is_debug() { debug_args } else { release_args };
     write_args(&gn_out_dir, args);
 
-    let status = Command::new(gn_path())
+    let path = env::current_dir().unwrap();
+    println!("The current directory is {}", path.display());
+    println!("gn gen --root={} {}", root, gn_out_dir.display());
+    let status = Command::new(gn())
       .arg(format!("--root={}", root))
       .arg("gen")
       .arg(&gn_out_dir)
@@ -42,7 +72,7 @@ pub fn build(target: &str) {
   // This helps Rust source files locate the snapshot, source map etc.
   println!("cargo:rustc-env=GN_OUT_DIR={}", gn_out_dir.display());
 
-  let status = Command::new(ninja_path())
+  let status = Command::new(ninja())
     .arg("-C")
     .arg(&gn_out_dir)
     .arg(target)
@@ -50,7 +80,7 @@ pub fn build(target: &str) {
     .expect("ninja failed");
   assert!(status.success());
 
-  rerun_if_changed(&ninja_path(), &gn_out_dir, target);
+  rerun_if_changed(&gn_out_dir, target);
 
   // TODO This is not sufficent. We need to use "gn desc" to query the target
   // and figure out what else we need to add to the link.
@@ -62,9 +92,9 @@ pub fn build(target: &str) {
 
 /// build.rs does not get re-run unless we tell cargo about what files we
 /// depend on. This outputs a bunch of rerun-if-changed lines to stdout.
-fn rerun_if_changed(ninja_path: &PathBuf, out_dir: &PathBuf, target: &str) {
+fn rerun_if_changed(out_dir: &PathBuf, target: &str) {
   // TODO(ry) `ninja -t deps` isn't sufficent. It doesn't capture runtime deps.
-  let deps = ninja_get_deps(ninja_path, out_dir, target);
+  let deps = ninja_get_deps(out_dir, target);
   for d in deps {
     let p = out_dir.join(d);
     debug_assert!(p.exists());
@@ -72,12 +102,8 @@ fn rerun_if_changed(ninja_path: &PathBuf, out_dir: &PathBuf, target: &str) {
   }
 }
 
-fn ninja_get_deps(
-  ninja_path: &PathBuf,
-  out_dir: &PathBuf,
-  target: &str,
-) -> HashSet<String> {
-  let output = Command::new(ninja_path)
+fn ninja_get_deps(out_dir: &PathBuf, target: &str) -> HashSet<String> {
+  let output = Command::new(ninja())
     .arg("-C")
     .arg(out_dir)
     .arg(target)
@@ -98,19 +124,4 @@ fn ninja_get_deps(
 fn write_args(path: &PathBuf, contents: &str) {
   fs::create_dir_all(path).expect("Unable to create gn_out directory");
   fs::write(path.join("args.gn"), contents).expect("Unable to write args.gn");
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn gn_exists() {
-    assert!(gn_path().exists());
-  }
-
-  #[test]
-  fn ninja_exists() {
-    assert!(ninja_path().exists());
-  }
 }
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v sccache; then
-  export RUSTC_WRAPPER="sccache"
-  export CXX="sccache clang++"
-fi
-
-cargo test -vv --all
