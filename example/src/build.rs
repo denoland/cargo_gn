4af7619183a8e8014a83cb70db5625d9b89c4a74
Ryan Dahl <ry@tinyclouds.org>
2019-07-30 18:17:49 -0400

    Use Vec<String> instead of tuples for GnArgs

--- example/src/build.rs
+++ example/src/build.rs
@@ -2,9 +2,9 @@ use cargo_gn;
 
 fn main() {
   let gn_args = if cargo_gn::is_debug() {
-    vec![("is_debug".to_string(), "true".to_string())]
+    vec!["is_debug=true".to_string()]
   } else {
-    vec![("is_debug".to_string(), "false".to_string())]
+    vec!["is_debug=false".to_string()]
   };
   let gn_out = cargo_gn::maybe_gen(".", gn_args);
   assert!(gn_out.exists());
