5072d137a908690dbfc026f1df9f66ccbdf950fa
Bert Belder <bertbelder@gmail.com>
2020-01-04 23:05:30 +0100

    Add workaround for windows gn rebase_path() bug

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,13 @@ use std::path::PathBuf;
 use std::process::Command;
 use std::process::Stdio;
 
-pub fn out_dir() -> PathBuf {
+#[derive(Clone, Debug)]
+struct Dirs {
+  pub out: PathBuf,
+  pub root: PathBuf,
+}
+
+fn get_dirs(manifest_dir: Option<&str>) -> Dirs {
   // The OUT_DIR is going to be a crate-specific directory like
   // "target/debug/build/cargo_gn_example-eee5160084460b2c"
   // But we want to share the GN build amongst all crates
@@ -13,15 +19,67 @@ pub fn out_dir() -> PathBuf {
   // directories.
   // TODO(ry) This is quite brittle - if Cargo changes the directory structure
   // this could break.
-  let out_dir = env::var("OUT_DIR").unwrap();
-  PathBuf::from(out_dir)
+  let out = env::var("OUT_DIR").map(PathBuf::from).unwrap();
+  let out = out
     .parent()
     .unwrap()
     .parent()
     .unwrap()
     .parent()
     .unwrap()
-    .to_owned()
+    .to_owned();
+
+  let root = match manifest_dir {
+    Some(s) => env::current_dir().unwrap().join(s),
+    None => env::var("CARGO_MANIFEST_DIR").map(PathBuf::from).unwrap(),
+  };
+
+  let mut dirs = Dirs { out, root };
+  maybe_symlink_root_dir(&mut dirs);
+  dirs
+}
+
+#[cfg(not(target_os = "windows"))]
+fn maybe_symlink_root_dir(_: &mut Dirs) {}
+
+#[cfg(target_os = "windows")]
+fn maybe_symlink_root_dir(dirs: &mut Dirs) {
+  // GN produces invalid paths if the source (a.k.a. root) directory is on a
+  // different drive than the output. If this is the case we'll create a
+  // symlink called "gn_root' in the out directory, next to 'gn_out', so it
+  // appears as if they're both on the same drive.
+  use std::fs::remove_dir;
+  use std::os::windows::fs::symlink_dir;
+  use std::path::{Component, Path};
+
+  let get_prefix = |p: &Path| {
+    p.components()
+      .find_map(|c| match c {
+        Component::Prefix(p) => Some(p),
+        _ => None,
+      })
+      .map(|p| p.as_os_str().to_owned())
+  };
+
+  let Dirs { out, root } = dirs;
+  if get_prefix(out) != get_prefix(root) {
+    let symlink = &*out.join("gn_root");
+    let target = &*root.canonicalize().unwrap();
+
+    println!("Creating symlink {:?} to {:?}", &symlink, &root);
+
+    loop {
+      match symlink.canonicalize() {
+        Ok(existing) if existing == target => break,
+        Ok(_) => remove_dir(symlink).expect("remove_dir failed"),
+        Err(_) => {
+          break symlink_dir(target, symlink).expect("symlink_dir failed")
+        }
+      }
+    }
+
+    dirs.out = symlink.to_path_buf();
+  }
 }
 
 pub fn is_debug() -> bool {
@@ -58,17 +116,22 @@ fn ninja(gn_out_dir: &PathBuf, maybe_env: Option<NinjaEnv>) -> Command {
 
 pub type GnArgs = Vec<String>;
 
-pub fn maybe_gen(root: &str, gn_args: GnArgs) -> PathBuf {
-  let gn_out_dir = out_dir().join("gn_out");
+pub fn maybe_gen(manifest_dir: &str, gn_args: GnArgs) -> PathBuf {
+  let dirs = get_dirs(Some(manifest_dir));
+  let gn_out_dir = dirs.out.join("gn_out");
 
   if !gn_out_dir.exists() || !gn_out_dir.join("build.ninja").exists() {
     let args = gn_args.join(" ");
 
     let path = env::current_dir().unwrap();
     println!("The current directory is {}", path.display());
-    println!("gn gen --root={} {}", root, gn_out_dir.display());
+    println!(
+      "gn gen --root={} {}",
+      dirs.root.display(),
+      gn_out_dir.display()
+    );
     let mut cmd = Command::new(gn());
-    cmd.arg(format!("--root={}", root));
+    cmd.arg(format!("--root={}", dirs.root.display()));
     cmd.arg("gen");
     cmd.arg(&gn_out_dir);
     cmd.arg("--args=".to_owned() + &args);
@@ -81,7 +144,7 @@ pub fn maybe_gen(root: &str, gn_args: GnArgs) -> PathBuf {
 }
 
 pub fn build(target: &str, maybe_env: Option<NinjaEnv>) {
-  let gn_out_dir = out_dir().join("gn_out");
+  let gn_out_dir = get_dirs(None).out.join("gn_out");
 
   // This helps Rust source files locate the snapshot, source map etc.
   println!("cargo:rustc-env=GN_OUT_DIR={}", gn_out_dir.display());
